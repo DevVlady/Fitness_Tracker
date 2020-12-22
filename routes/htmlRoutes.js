8209709fda21ec44104e073c1f6931da4f12e717
Vladimir <vladimir.s.dimitrov14@gmail.com>
2020-12-21 19:43:18 -0600

    Added the root route in the html routes file.

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -2,5 +2,9 @@
 const path = require("path");
 const router = require("express").Router();
 
+//Route to home page(root route)
+router.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/index.html"));
+});
 
 module.exports = router;
