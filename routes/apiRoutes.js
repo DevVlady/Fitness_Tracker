92d9109b1ef3ed2a4c1d6807012163d0b22e8a9b
Vladimir <vladimir.s.dimitrov14@gmail.com>
2020-12-21 19:59:23 -0600

    Fixed the put method in the api routes as it had the get method instead. I also added console logs to see where my error is as one of my routes is not functioning properly.

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -5,6 +5,7 @@ const db = require("../models");
 
 //Route to create a new workout
 router.post("/api/workouts", ({ body }, res) => {
+    console.log('**POST**/api/workouts**')
     Workout.create(body)
         .then(dbWorkout => {
             res.json(dbWorkout);
@@ -15,7 +16,8 @@ router.post("/api/workouts", ({ body }, res) => {
 });
 
 //Add details to a workout
-router.post("/api/workouts/:id", (req, res) => {
+router.put("/api/workouts/:id", (req, res) => {
+    console.log('**PUT**/api/workouts/:id**')
     Workout.findByIdAndUpdate(
         req.params.id,
         { $push: { exercises: req.body } },
@@ -31,6 +33,7 @@ router.post("/api/workouts/:id", (req, res) => {
 
 //Route to find existing workouts in database
 router.get("/api/workouts", (req, res) => {
+    console.log('**GET**/api/workouts**')
     Workout.find({})
         .sort({ date: -1 })
         .then(dbWorkout => {
@@ -43,6 +46,7 @@ router.get("/api/workouts", (req, res) => {
 
 //Route to find existing workouts in database
 router.get("/api/workouts/range", (req, res) => {
+    console.log('**GET**api/workouts/range**')
     Workout.find({}, (err, data) => {
         if (err) {
             res.status(400).json(err);
--- server.js
+++ server.js
@@ -13,8 +13,8 @@ app.use(express.json());
 app.use(express.static("public"));
 
 //Routes
-// app.use(require("./routes/apiRoutes.js"));
-// app.use(require("./routes/htmlRoutes.js"));
+app.use(require("./routes/apiRoutes.js"));
+app.use(require("./routes/htmlRoutes.js"));
 
 //Establish connection
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/fitnessApp', {
