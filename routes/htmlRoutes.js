36495638b16ab7c88a4fc886b59505a73007c88c
Vladimir <vladimir.s.dimitrov14@gmail.com>
2020-12-21 19:48:01 -0600

    Added the console log so that i can easily identify if the route is being hit properly.

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -4,11 +4,13 @@ const router = require("express").Router();
 
 //Route to home page(root route)
 router.get("/", (req, res) => {
+    console.log('**/**index.html route**');
     res.sendFile(path.join(__dirname, "../public/index.html"));
 });
 
 //Route to the stats page
 router.get("/stats", (req, res) => {
+    console.log('**/stats**stats.html route**')
     res.sendFile(path.join(__dirname, "../public/stats.html"));
 });
 
